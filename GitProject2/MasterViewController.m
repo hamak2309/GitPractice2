1874f90d4ba31812fae33ede4483fcaffba4ccc7
hamak2309 <hama2309@gmail.com>
2015-05-20 00:39:04 +0900

    ローカルfeatureBranchからリモートfeatureBranchへのコミット漏れをコミット

--- GitProject2/MasterViewController.m
+++ GitProject2/MasterViewController.m
@@ -33,6 +33,8 @@
     //featureBranch2を更新
 //featureBranch2を更新
 
+//devからfeatureへMerge後に、featureを更新
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
