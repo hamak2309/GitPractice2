f02efc3a4acca550e8e7e751e2e6e3e9543e25dc
hamak2309 <hama2309@gmail.com>
2015-05-19 19:22:29 +0900

    devを更新

--- GitProject2/MasterViewController.m
+++ GitProject2/MasterViewController.m
@@ -28,7 +28,8 @@
 
     UIBarButtonItem *addButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(insertNewObject:)];
     self.navigationItem.rightBarButtonItem = addButton;
-}
+    //devを更新
+}//devを更新
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
